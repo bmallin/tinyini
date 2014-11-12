32dc69c66d147e7970249a7812787ec9e66aa247
Benjamin Mallin <b.m.mallin@gmail.com>
2014-11-11 21:39:00 -0500

    Simplify logic.

--- tinyini.py
+++ tinyini.py
@@ -1,3 +1,30 @@
+"""tinyini.py
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Benjamin Mallin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+
 def parse(filepath):
     with open(filepath, 'r', encoding='utf-8') as fp:
         config = {}
@@ -14,11 +41,9 @@ def parse(filepath):
                 section = line[1:-1]
                 config[section] = {}
                 last_section = section
-            elif line.find('=') > -1:
-                key, value = map(str.strip, line.split('='))
-                config[last_section][key] = value
-            elif line.find(':') > -1:
-                key, value = map(str.strip, line.split(':'))
+            elif line.find('=') > -1 or line.find(':') > -1:
+                key, value = map(str.strip, max(line.split('='),
+                                                line.split(':'), key=len))
                 config[last_section][key] = value
             else:
                 config[last_section][line] = None
